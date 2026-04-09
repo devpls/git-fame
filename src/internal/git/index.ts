3c149ad6bae12a034d90c5569190dfcb485dc49c
Mykhailo Kalashnikov <mihal.kalashnikov@gmail.com>
2026-04-09 11:18:42 +0200

    Add discoverSubmodules helper

--- src/internal/git/index.ts
+++ src/internal/git/index.ts
@@ -1,5 +1,6 @@
 export { assertGitInstalled } from './assert-git-installed.js';
 export { collectStream } from './collect-stream.js';
+export { discoverSubmodules, type SubmoduleInfo } from './discover-submodules.js';
 export { isGitRepo } from './is-git-repo.js';
 export { listTrackedFiles } from './list-tracked-files.js';
 export { resolveRange, type Range, type ResolvedRange } from './resolve-range.js';
